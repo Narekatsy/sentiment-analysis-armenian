54487efbed60d7d4cf13c6532d4d33c91f55945b
Narek <44674958+Narekatsy@users.noreply.github.com>
2023-04-27 16:37:22 +0400

    Add files via upload

---
+++ sentiment_analyzer.py
@@ -0,0 +1,67 @@
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import xml_parser as ps
+from scipy.interpolate import interp1d
+import numpy as np
+import matplotlib.pyplot as plt
+
+analyzer = SentimentIntensityAnalyzer()
+
+# Apply sentiment analysis on translations column
+def get_avg_sentiment(translations):
+    scores = []
+    
+    for t in translations:
+        # join all translations into a single string
+        t_str = ", ".join(t)
+        # compute sentiment score for the string
+        score = analyzer.polarity_scores(t_str)
+        scores.append(score["compound"])
+        
+    return scores
+
+def split_and_analyze(df, num_splits = 20):
+    # Combine all lists in dataframe into a single list of sentences
+    all_sentences = [' '.join(row) for row in df]
+    
+    # Split the sentences into num_splits parts
+    chunk_size = len(all_sentences) // num_splits
+    sentence_chunks = [all_sentences[i:i+chunk_size] for i in range(0, len(all_sentences), chunk_size)]
+    
+    # Analyze the sentiment of each sentence chunk
+    sentiment_scores = []
+    for chunk in sentence_chunks:
+        chunk_sentence = " ".join(chunk)
+        sentiment_scores.append(analyzer.polarity_scores(chunk_sentence)['compound'])
+        #print(f"{chunk_sentence}\n LENGTH: {len(chunk_sentence)}")
+        
+    return sentiment_scores
+
+
+if __name__ == "__main__":
+    book_title = "Tumanyan_tales2.htm"
+    
+    df = ps.extract_words(book_title)
+    df = ps.remove_title(df)
+    
+    book_translation = df['translation']
+    get_avg_sentiment(book_translation)
+
+    scores = split_and_analyze(book_translation, 10)
+    x = range(len(scores))
+    
+    f = interp1d(x, scores, kind='cubic')
+
+    # Create an array of points to evaluate the interpolator
+    x_interp = np.linspace(0, len(scores) - 1, 100)
+    
+    # Evaluate the interpolator at the points
+    y_interp = f(x_interp)
+    
+    plt.figure(figsize=(12, 8))
+    plt.plot(x, scores, 'o', label='Sentiment Scores')
+    plt.plot(x_interp, y_interp, label='Interpolated Curve')
+    plt.xlabel('Part Number')
+    plt.ylabel('Sentiment Score')
+    plt.title("Հեղինակ - վերնագիր")
+    plt.show()
+    
---
+++ xml_parser.py
@@ -0,0 +1,87 @@
+import html
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+def extract_words(book_name):
+    # Read the HTML file
+    with open(book_name, 'r', encoding='utf-8') as f:
+        contents = f.read()
+    
+    # Convert HTML entities to their corresponding characters
+    contents = html.unescape(contents)
+    
+    # Parse the HTML file with BeautifulSoup
+    soup = BeautifulSoup(contents, "html.parser")
+    
+    # Extract the XML tags for each word
+    words = soup.find_all("span")
+    
+    # Extract the lemma and definition for each word
+    arm_words = []
+    pos = []
+    form = []
+    translation = []
+    
+    for word in words:
+        arm_word = word.text.strip()
+        arm_words.append(arm_word)
+        titles = word.get("titles")
+        
+        if titles:
+            titles = titles.split("	")
+            
+            try:
+                pos_form = titles[0].split()[1:]
+            except:
+                pass
+            
+            try:
+                if str(pos_form[0]) == str(pos_form[0]).upper():
+                    pos.append(pos_form[0])
+            except:
+                pos.append(None)
+                pass
+            
+            try:
+                translation.append(titles[-1].split(","))            
+            except:
+                translation.append(None)
+                pass
+            
+            try:
+                form.append(pos_form[1])
+            except:
+                form.append(None)
+                pass
+    
+        else:
+            pos.append(None)
+            form.append(None)
+            translation.append(None)
+    
+    # Modifying lists for better look
+    for i in range(len(pos)):
+        if pos[i] is not None:
+            pos[i] = pos[i].replace('(', '').replace(')', '')
+            
+    for i in range(len(form)):
+        if form[i] is not None:
+            form[i] = form[i].replace('(', '').replace(')', '')
+
+
+    # Create a dictionary with the lists
+    data = {"word": arm_words, "pos": pos, "form": form, "translation": translation}
+
+    # create a dataframe from the dictionary
+    df = pd.DataFrame(data)
+    
+    return df
+
+def remove_title(df):
+    # Remove the title, drop the rows with none values
+    df = df.drop(index=range(3))
+    df = df.dropna(axis=0, thresh=2)
+    
+    return df
+
